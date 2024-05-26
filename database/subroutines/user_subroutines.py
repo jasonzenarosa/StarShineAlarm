1fdd11b360cf00697be2a488b7901ce114361eab
Jason Zenarosa <jasonzenarosa@gmail.com>
2024-05-25 18:35:10 -0700

    created subroutine for verifying user/password pair

--- database/subroutines/user_subroutines.py
+++ database/subroutines/user_subroutines.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+USERNAME = 0
+PASSWORD = 1
+
 def create_user(username:str, password:int):
     '''returns 0 on success and 1 if username is taken'''
 
@@ -17,18 +20,50 @@ def create_user(username:str, password:int):
         '''
         args = (username, password, 'english', 'pst', 'venushacks', 0)
         cursor.execute(cmd, args)
-        err = 0
+        code = 0
 
     else:
         # username is not available
-        err = 1
+        code = 1
 
     cursor.close()
     conn.commit()
     conn.close()
 
-    return err
+    return code
+
+def verify_user(username:str, password:int):
+    '''return 0 on success, 1 if user doesn't exist, 2 if password is incorrect'''
+    conn = sqlite3.connect('database/StarShine.db')
+    cursor = conn.cursor()
+
+    cmd = 'SELECT * FROM Users WHERE username = ?'
+    cursor.execute(cmd, (username,))
+    row = cursor.fetchone()
+
+    code = 0
+
+    if row is None:
+        code = 1
+    
+    if row and (row[PASSWORD] != password):
+        code = 2
+    
+    return code
 
 if __name__ == "__main__":
-    err = create_user("test_username", "test_password")
-    print(f"Error Code: {err}")
+    print(f"Creating user: test_username, test_password")
+    code = create_user("test_username", "test_password")
+    print(f"Code: {code}")
+
+    print(f"Verifying user: test_username, test_password")
+    code = verify_user("test_username", "test_password")
+    print(f"Code: {code}")
+
+    print(f"Verifying user: wrong_username, wrong_password")
+    code = verify_user("wrong_username", "wrong_password")
+    print(f"Code: {code}")
+
+    print(f"Verifying user: test_username, wrong_password")
+    code = verify_user("test_username", "wrong_password")
+    print(f"Code: {code}")
